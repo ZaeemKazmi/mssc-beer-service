54a1852ea087c2c742c8f0baa28f89055f48d0f7
ZaeemHasan <syedzaeem25@gmail.com>
2020-11-13 20:10:24 +0100

    Spring REST docs added

--- src/main/java/com/zk/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/zk/msscbeerservice/web/controller/BeerController.java
@@ -1,34 +1,48 @@
 package com.zk.msscbeerservice.web.controller;
 
+import com.zk.msscbeerservice.repositories.BeerRepository;
+import com.zk.msscbeerservice.web.mappers.BeerMapper;
 import com.zk.msscbeerservice.web.model.BeerDto;
-import com.zk.msscbeerservice.web.model.BeerStyleEnum;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.UUID;
 
-@RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/beer")
+@RestController
 public class BeerController {
+
+    private final BeerMapper beerMapper;
+    private final BeerRepository beerRepository;
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
 
-        return new ResponseEntity<>(BeerDto.builder().id(beerId).beerName("Ginger Ale").beerStyle(BeerStyleEnum.ALE).build(), HttpStatus.OK);
+        return new ResponseEntity<>(beerMapper.beerToBeerDto(beerRepository.findById(beerId).get()), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@Valid @RequestBody BeerDto beerDto){
-        // todo impl
+    public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto){
+
+        beerRepository.save(beerMapper.beerDtoToBeer(beerDto));
 
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto){
-        // todo impl
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beerDto){
+        beerRepository.findById(beerId).ifPresent(beer -> {
+            beer.setBeerName(beerDto.getBeerName());
+            beer.setBeerStyle(beerDto.getBeerStyle().name());
+            beer.setPrice(beerDto.getPrice());
+            beer.setUpc(beerDto.getUpc());
+
+            beerRepository.save(beer);
+        });
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
--- src/main/java/com/zk/msscbeerservice/web/model/BeerStyleEnum.java
+++ src/main/java/com/zk/msscbeerservice/web/model/BeerStyleEnum.java
@@ -1,6 +1,5 @@
 package com.zk.msscbeerservice.web.model;
 
 public enum BeerStyleEnum {
-
-    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE,  SAISON
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
 }
